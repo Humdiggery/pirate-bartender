d41c0532b468bf58cf1b16fc15137a3a5ebd07e4
humdiggery <kriley277@yahoo.com>
2016-07-09 11:17:53 +0000

    basic requirements met

--- pirate_bartend.py
+++ pirate_bartend.py
@@ -2,9 +2,12 @@
 
 #Use version control on github. Push final project to github and share with mentor
 
+import random
+
 #Setup
 #neccessary parameters
 
+print("Ahoy Matey! Welcome to the best bar on the seven seas! Let's get ye a drink. \n")
 questions = {
     "strong": "Do ye like yer drinks strong?",
     "salty": "Do ye like it with a salty tang?",
@@ -24,6 +27,10 @@ ingredients = {
 #questions to determine customer taste
 #identify ingredients that match that taste
 
+#write a function that requests what style the customer wants
+#function should ask all questions in the question dictionary
+#utilize input(). Error exceptions for things that don't exist. 
+
 def customer_pref():
     preference={}
     for key in questions:
@@ -35,25 +42,38 @@ def customer_pref():
     #answers can be boolean values
     return preference
 
-output=customer_pref()
-print(output)
-    
-#write a function that requests what style the customer wants
-#function should ask all questions in the question dictionary
-#utilize input(). Error exceptions for things that don't exist. 
 
-#def create_drink():
+    
+def create_drink(preference):
 #write a function to construct a drink
-#create an empty list to represent the drink
+
+    drink = []
+    for key in preference:
+        if preference[key]==True:
+            drink.append(random.choice(ingredients[key]))
+    return drink        
+            
+
+
+
+
+def main():
+    answer=customer_pref()
+    #print (answer) #for testing
+    
+    for drink in create_drink(answer):
+        print("it's got "+drink)
+    
+    
+
 #append corresponding ingredients from the dictionary for each ingredients
     #the customer said they liked
 #use random.choice() to select a random ingredient
 
 #Provide a main function to call function 1 & 2 in order and passing
 #the list of drink preferences to the drink creation function
-#if __name__=='__main__':
-    #customer_pref()
-    #create_drink()
+if __name__=='__main__':
+    main()
 
 
 
